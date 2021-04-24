af88a3c4e30dc40f9325355838106c8ab8070b3f
intel193 <nanahara1930@yahoo.co.jp>
2021-04-25 02:49:59 +0900

    no message

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,54 +1,59 @@
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap Sample</title>
-    <!-- BootstrapのCSS読み込み -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- jQuery読み込み -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- BootstrapのJS読み込み -->
-	<script src="js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="css/sample.css">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap Sample</title>
+  <!-- BootstrapのCSS読み込み -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- jQuery読み込み -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- BootstrapのJS読み込み -->
+  <script src="js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/sample.css">
   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <!-- ここから -->
-    <div class="sticky-top">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-5" id="textBox">漫画家になろう</div>       
-          
-          <div class="col-md-3" id="toptext">
-            <form method="get" action="#" class="search_container">
+</head>
+
+<body>
+  <!-- ここから -->
+  <div class="sticky-top" id="header">
+    <div class="container1">
+      <div class="row">
+        <div class="col-md-8" id="textBox">漫画家になろう</div>
+
+        <div class="col-md-2" id="toptext">
+          <form method="get" action="#" class="search_container">
             <input type="text" size="30" placeholder="作品名、作家名を入力してください">
             <input type="submit" value="&#xf002">
-            </form>
-          </div>
-
-          <div class="col-md-4">
-            <div class="row">
-            <div class="col-md-3" id="textBox">新規登録</div>
-            <div class="col-md-2" id="textBox">ログイン</div>
-            </div>
-          </div> 
-
+          </form>
         </div>
+
+        <div class="col-md-2">
+          <div class="row">
+            <div class="col-md-5" id="textBox"></div>
+            <div class="col-md-4" id="textBox">新規登録</div>
+            <div class="col-md-3" id="textBox">ログイン</div>
+          </div>
+        </div>
+
       </div>
     </div>
-    <img src="img/anime.jpg" alt="海の写真" title="空と海" id="pictbox">
-      
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  </body>
+  </div>
+
+  <img src="img/anime.jpg" alt="海の写真" title="空と海" id="pictbox">
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
 </html>
